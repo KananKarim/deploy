f7b0b2c1bea890c58bc14b920611ff1f8fc95b46
KananKarim <kenankerimli7770@gmail.com>
2023-10-03 20:03:15 +0400

    post method

--- app.js
+++ app.js
@@ -17,12 +17,20 @@ const pets = [
     }
 ]
 
+app.use(express.json())
+
 app.get("/pets",(req,res) => {
     return res.json(pets)
 })
 
 app.get("/pets/:id",(req,res) => {
     return res.json(pets.find(pet => pet.id === +req.params.id) )
+})
+
+app.post("/pets",(req,res) => {
+    const id = Date.now();
+    pets.push({...JSON.parse(req.body),id})
+    return res.json("Pets successfully added!")
 })
 
 app.listen(3000,() => {
